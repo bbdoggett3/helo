5846c39c088c5e0ab8cfad29e59e7cd2e9f762a1
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-07 20:08:28 -0600

    create styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -11,7 +11,7 @@ function Nav(props) {
       <div>
         <nav className = "navbar-container">
             <img className="profile-pic" src="https://robohash.org/Harry" alt = "profile of a person"></img>
-            <h3>Welcome back {props.username}</h3>
+            <h3>{props.username}</h3>
             <Link to='/dashboard'><span><img className="icon-pic" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fhome%20button.png?v=1591411136554"  alt="house icon"/></span></Link>
             <Link to='/new'><span><img className="icon-pic" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Fnew%20file%20folder.png?v=1591412181828" alt="new post icon"/></span></Link>
             <Link to='/'><span><img className="logout-btn" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2Flogout%20button.png?v=1591412517415" alt="logout button"/></span></Link>
